9f8ca9d5c888a919bee2e840ae0e20025ccdf483
Sunakshi Zutshi <zutshisunakshi@github.com>
2025-06-19 13:40:36 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("hi")
+
+function setLanguageOptions() {
+    var href = document.location.href,
+        deflt = href.substring(href.indexOf("default=")+8);
+    document.write("<OPTION value=1>"+deflt+"</OPTION>");
+    document.write("<OPTION value=2>English</OPTION>");
+}
